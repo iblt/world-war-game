5e1a9543a032e3b22f8b01150d3f6087f3fe2c3b
Иван Беляев <i.beliaev@friendlee.ru>
2026-03-24 13:13:14 +0700

    fix: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
 	{
-		ignorePatterns: ['src/generated/prisma-client', '.next'],
+		ignores: ['src/generated/prisma-client', '.next'],
 	},
 ]
 
